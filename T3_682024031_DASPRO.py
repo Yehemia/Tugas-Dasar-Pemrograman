471325e8846b1870192b880fdc0a53eea49ca022
Yehemia <682024031@student.uksw.edu>
2024-09-17 17:41:35 +0700

    Add files via upload

--- T3_682024031_DASPRO.py
+++ T3_682024031_DASPRO.py
@@ -1,9 +1,12 @@
+# Struk kasir
+
 print('Selamat datang di Kupi')
 print('Get discount 30% for minimum order 50K')
 print("--------------------------------------")
 
-nama = input('silahkan masukan Nama anda = ')
+nama = input('silahkan masukan Nama anda = ') #input nama pembeli
 
+# Dictionary menu 
 menu = {
     "Salad": 15000,
     "Americano": 20000,
@@ -18,11 +21,13 @@ print("=========================================")
 for i in menu:
     print("daftar menu : ", i,"\t harga : Rp.", menu[i])
 
-pesanan = []
-total_bayar = 0
+pesanan = [] #List untuk menyimpan pesanan
+total_bayar = 0 #variabel untuk menyimpan total harga pembelian
 
+#untuk membuat lebih dari 1 pesanan
 while True:
     beli = input("Masukkan menu yang ingin anda beli: ").title()
+    #untuk mengecek apakah menu ada
     if beli not in menu:
         print("Maaf menu tidak ada")
         continue
@@ -34,14 +39,14 @@ while True:
     pesanan.append({
         "menu": beli,
         "jumlah": jumlah,
-        "subtotal": bayar
+        "subtotal": bayar 
     })
     
     lanjut = input("Apakah ingin menambah pesanan lain? (y/n): ").lower()
     if lanjut == 'n':
         break
 
-
+#untuk mengecek diskon
 if total_bayar > 50000:
     diskon = total_bayar * 30/100
     total_setelah_diskon = total_bayar - diskon
@@ -49,7 +54,7 @@ else:
     diskon = 0
     total_setelah_diskon= total_bayar   
 
-
+# Cetak struk pemjualan
 print("==========Detail Pembayaran===========")
 print("--------------------------------------")
 print('HAI KUPIERS!!!')           
